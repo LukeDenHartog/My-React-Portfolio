1c718532744113537fd2d76b1b6355308e2a9e36
Luke <121516961+Afrozez@users.noreply.github.com>
2023-08-24 19:29:07 -0700

    added a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer(props) {
+    return  (
+        <footer className="footer">
+            <p>© 2023 Luke A DenHartog. All Rights Reserved.</p>
+            {props.children}
+      </footer>
+    )
+}
