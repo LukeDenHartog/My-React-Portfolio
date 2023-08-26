7835917aa2f09493d6b789c0f478dd86f891480a
Luke <121516961+Afrozez@users.noreply.github.com>
2023-08-26 14:05:35 -0700

    added an array of project objects and mapped over to display each

--- src/components/ContentFolder/Portfolio.js
+++ src/components/ContentFolder/Portfolio.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import codingQuizPic from './projectImages/coding-quiz.jpg'
+import weatherAppPic from './projectImages/weather-dashboard-picture.jpg'
 
 export default function Portfolio() {
-    return <h1>Projects</h1>
+    const portfolioProjects = [
+        {
+          image: codingQuizPic,
+          title: "Coding Quiz",
+          deployedLink: "https://afrozez.github.io/coding-quiz/",
+          githubRepo: "https://github.com/Afrozez/coding-quiz"
+        },
+        {
+          image: weatherAppPic,
+          title: "Weather App",
+          deployedLink: "https://afrozez.github.io/weather-dashboard/",
+          githubRepo: "https://github.com/Afrozez/weather-dashboard"
+        },
+
+      ];
+      
+
+      
+      return (
+        <div>
+            <h1>Projects</h1>
+            {portfolioProjects.map(project => (
+                <div key={project.title} className="projectFlexContainer">
+                    <h3>{project.title}</h3>
+                    <a href={project.deployedLink}>
+                        <img src={project.image} alt={project.title} style={{ width: '200px', height: '150px' }} />
+                    </a>
+                    <a href={project.githubRepo}>GitHub Repo</a>
+                    <a href={project.deployedLink}>Visit Deployed App</a>
+                </div>
+            ))}
+        </div>
+    );
 }
